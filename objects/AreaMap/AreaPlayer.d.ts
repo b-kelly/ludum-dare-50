01418a8f613be49b8b81437465d040c8da94dfac
b-kelly <b-kelly@users.noreply.github.com>
2022-04-02 19:38:47 +0000

    Deploying to gh-pages from @ b-kelly/ludum-dare-50@6864a8ed893292134c00f2d42b693bd6cc784a0b 🚀

---
+++ objects/AreaMap/AreaPlayer.d.ts
@@ -0,0 +1,6 @@
+export declare class AreaPlayer extends Phaser.GameObjects.Rectangle {
+    body: Phaser.Physics.Arcade.Body;
+    private controls;
+    constructor(scene: Phaser.Scene, x: number, y: number);
+    update(): void;
+}
--- scenes/ExploreScene.d.ts
+++ scenes/ExploreScene.d.ts
@@ -3,8 +3,11 @@ export declare class ExploreScene extends CustomScene {
     static readonly KEY = "ExploreScene";
     private map;
     private tileMap;
+    private player;
     constructor();
     init(data: object): void;
     preload(): void;
     create(): void;
+    update(): void;
+    private translateCoord;
 }
