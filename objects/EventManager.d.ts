f18b732396afd8f20aa02965d911b4204ecbbf2e
b-kelly <b-kelly@users.noreply.github.com>
2022-04-03 04:42:53 +0000

    Deploying to gh-pages from @ b-kelly/ludum-dare-50@9eb920bcb05a71ccc4b1d3fb3330f836ab1c10a0 🚀

--- objects/EventManager.d.ts
+++ objects/EventManager.d.ts
@@ -1,6 +1,13 @@
 import { CustomScene } from "./CustomScene";
 import { Resources } from "./GlobalDataStore";
+/**
+ * none - can happen any time
+ * daily - only happens on daily review
+ * map - only happens as a random map event
+ */
+declare type EventType = "none" | "daily" | "map";
 export interface GameEvent {
+    type: EventType;
     shortDescriptor: string;
     message: string;
     resourceDelta: Partial<Resources>;
@@ -14,6 +21,9 @@ export declare class EventManager {
     private scene;
     constructor(scene: CustomScene);
     spawnDailyEvent(): EventOutcome;
+    spawnMapEvent(): EventOutcome;
+    private spawnEvent;
     private applyEvent;
     private chooseEvent;
 }
+export {};
