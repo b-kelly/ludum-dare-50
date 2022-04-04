2405fa372db9536d8af0b60cd43047b82bb90b22
kristinalustig <kristina.lustig@gmail.com>
2022-04-04 19:53:30 +0000

    Deploying to gh-pages from @ b-kelly/ludum-dare-50@335841ec270c17aac43a4fe3e317daa66d4cee77 🚀

--- objects/AreaMap/AreaCreature.d.ts
+++ objects/AreaMap/AreaCreature.d.ts
@@ -1,7 +1,7 @@
 import { CellBiome } from "../WorldMap/shared";
 export declare class AreaCreature extends Phaser.GameObjects.Sprite {
     static readonly DAMAGE_AMOUNT = 1;
-    static readonly SPEED = 50;
+    static readonly SPEED = 200;
     static readonly MOVE_DURATION_MS = 5000;
     body: Phaser.Physics.Arcade.Body;
     private lastDirectionChange;
