1df81e9396fdcc220dd123e137cd655c977d6592
b-kelly <b-kelly@users.noreply.github.com>
2022-04-04 20:42:03 +0000

    Deploying to gh-pages from @ b-kelly/ludum-dare-50@05716c404de3b177e25b61aa0711a067bf221204 🚀

--- objects/CustomScene.d.ts
+++ objects/CustomScene.d.ts
@@ -4,11 +4,13 @@ import { GlobalDataStore } from "./GlobalDataStore";
 export declare class CustomScene extends Phaser.Scene {
     global: GlobalDataStore;
     eventManager: EventManager;
+    private backgroundMusic;
     get bounds(): {
         width: number;
         height: number;
     };
     constructor(config: string | Phaser.Types.Scenes.SettingsConfig);
     init(data: object): void;
+    playBgm(key: string, extra?: Phaser.Types.Sound.SoundConfig | Phaser.Types.Sound.SoundMarker): boolean;
     fadeToScene(target: string, data?: object, keepAudioPlaying?: boolean): void;
 }
